ede86d9163872bc4b7e8db5a4fbb3c1fd0bdf2f8
B4tiste <64788456+B4tiste@users.noreply.github.com>
2021-03-01 16:58:42 +0100

    Add files via upload

--- ia_maze.py
+++ ia_maze.py
@@ -1,14 +1,11 @@
-# Maze generator -- Randomized Prim Algorithm
-
-## Imports
+from PIL import Image, ImageDraw
 import random
 import time
 import os
 import numpy as np
 from colorama import init
 from colorama import Fore, Back, Style
-
-# Functions
+images = []
 
 clear = lambda : os.system('clear')
 
@@ -49,13 +46,32 @@ def surroundingCells(rand_wall):
 wall = 'w'
 cell = 'c'
 unvisited = 'u'
-height = 15
-width = 15
+height = 5
+width = 5
 maze = []
+zoom = 20
+borders = 6
+start = (0,1)
+end = [0,0]
 
-# Initialize colorama
+a = [[0]*width]*height
+
 init()
 
+"""
+a = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0 ,0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0 ,0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0 ,0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0 ,0, 0, 0, 1, 0, 1, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0 ,1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0 ,0, 0, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0 ,0, 1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0 ,0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+]
+"""
 # Denote all cells as unvisited
 for i in range(0, height):
 	line = []
@@ -251,21 +267,152 @@ for i in range(0, height):
 		if (maze[i][j] == 'u'):
 			maze[i][j] = 'w'
 
+
 # Set entrance and exit
+
+for i in range(0, width):
+    if maze[1][i] == 'c':
+        maze[0][i] = 'c'
+        end[0] = i
+
+        break
+
+for i in range(width - 1, 0, -1):
+    if maze[height-2][i] == 'c':
+        maze[height-1][i] = 'c'
+        end[1] = i
+
+        break
+
+"""
 for i in range(0, width):
 	if (maze[1][i] == 'c'):
 		maze[0][i] = 'c'
+        end[0] = i
+
 		break
 
 for i in range(width-1, 0, -1):
 	if (maze[height-2][i] == 'c'):
 		maze[height-1][i] = 'c'
-		break
+        end[1] = i
 
-# Print final maze
-#print(np.matrix(maze))
+		break
+"""
+
+print(np.matrix(a))
+printMaze(maze)
+print(height)
+print(width)
+
+for y in range(height):
+    for e in range(width):
+        if maze[y][e] == 'w':
+            a[y][e] = 1
+        if maze[y][e] == 'c':
+            a[y][e] = 0
+
+print(np.matrix(a))
+
+def make_step(k):
+  for i in range(len(m)):
+    for j in range(len(m[i])):
+      if m[i][j] == k:
+        if i>0 and m[i-1][j] == 0 and a[i-1][j] == 0:
+          m[i-1][j] = k + 1
+        if j>0 and m[i][j-1] == 0 and a[i][j-1] == 0:
+          m[i][j-1] = k + 1
+        if i<len(m)-1 and m[i+1][j] == 0 and a[i+1][j] == 0:
+          m[i+1][j] = k + 1
+        if j<len(m[i])-1 and m[i][j+1] == 0 and a[i][j+1] == 0:
+           m[i][j+1] = k + 1
+
+def print_m(m):
+    for i in range(len(m)):
+        for j in range(len(m[i])):
+            print( str(m[i][j]).ljust(2),end=' ')
+        print()
+
+def draw_matrix(a,m, the_path = []):
+    im = Image.new('RGB', (zoom * len(a[0]), zoom * len(a)), (255, 255, 255))
+    draw = ImageDraw.Draw(im)
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            color = (255, 255, 255)
+            r = 0
+            if a[i][j] == 1:
+                color = (0, 0, 0)
+            if i == start[0] and j == start[1]:
+                color = (0, 255, 0)
+                r = borders
+            if i == end[0] and j == end[1]:
+                color = (0, 255, 0)
+                r = borders
+            draw.rectangle((j*zoom+r, i*zoom+r, j*zoom+zoom-r-1, i*zoom+zoom-r-1), fill=color)
+            if m[i][j] > 0:
+                r = borders
+                draw.ellipse((j * zoom + r, i * zoom + r, j * zoom + zoom - r - 1, i * zoom + zoom - r - 1),
+                               fill=(255,0,0))
+    for u in range(len(the_path)-1):
+        y = the_path[u][0]*zoom + int(zoom/2)
+        x = the_path[u][1]*zoom + int(zoom/2)
+        y1 = the_path[u+1][0]*zoom + int(zoom/2)
+        x1 = the_path[u+1][1]*zoom + int(zoom/2)
+        draw.line((x,y,x1,y1), fill=(255, 0,0), width=5)
+    draw.rectangle((0, 0, zoom * len(a[0]), zoom * len(a)), outline=(0,255,0), width=2)
+    images.append(im)
+
+
+m = []
+for i in range(len(a)):
+    m.append([])
+    for j in range(len(a[i])):
+        m[-1].append(0)
+i,j = start
+m[i][j] = 1
+
+k = 0
+while m[end[0]][end[1]] == 0:
+    k += 1
+    make_step(k)
+    draw_matrix(a, m)
+
+
+i, j = end
+k = m[i][j]
+the_path = [(i,j)]
+while k > 1:
+  if i > 0 and m[i - 1][j] == k-1:
+    i, j = i-1, j
+    the_path.append((i, j))
+    k-=1
+  elif j > 0 and m[i][j - 1] == k-1:
+    i, j = i, j-1
+    the_path.append((i, j))
+    k-=1
+  elif i < len(m) - 1 and m[i + 1][j] == k-1:
+    i, j = i+1, j
+    the_path.append((i, j))
+    k-=1
+  elif j < len(m[i]) - 1 and m[i][j + 1] == k-1:
+    i, j = i, j+1
+    the_path.append((i, j))
+    k -= 1
+  draw_matrix(a, m, the_path)
+
+for i in range(10):
+    if i % 2 == 0:
+        draw_matrix(a, m, the_path)
+    else:
+        draw_matrix(a, m)
+
+#print_m(m)
+#print(the_path)
+
+
+images[0].save('gif_maze.gif',
+               save_all=True, append_images=images[1:],
+               optimize=False, duration=1, loop=0)
+
 print('\n')
 printMaze(maze)
-print(Fore.WHITE)
-print(type(maze))
-#print(maze)
